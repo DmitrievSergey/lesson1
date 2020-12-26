0d6fe5b4333917b7873b6ab0c692c3e4b4dddb86
Sergey <sergei2003@bk.ru>
2020-12-26 13:25:21 +0300

    add
    забыл добавить изменения

--- l1_DmitrievSergey.playground/Contents.swift
+++ l1_DmitrievSergey.playground/Contents.swift
@@ -1,3 +1,52 @@
 import UIKit
 
-var str = "Hello, playground"
+//1.Решить квадратное уравнение. ax^2+bx+c
+//-b +- sqrt(b2 - 4ac)/2a
+let a: Double = 2.5
+let b: Double = 3.5
+let c: Double = 4.5
+//Определяем дискриминант
+var D = b*b - 4*a*c
+
+if D > 0 {
+    
+    let x1: Double = (sqrt(D) - b)/(2*a)
+    let x2: Double = (-sqrt(D) - b)/(2*a)
+    print(x1)
+    print(x2)
+} else if D == 0 {
+    let x = -b/2*a
+    print(x)
+} else {
+    print("Нет решения")
+}
+//2. Даны катеты d и e прямоугольного треугольника. Найти площадь, периметр и гипотенузу f треугольника.
+// Площадь S = d*e/2
+// Периметр P = d + e + f
+// Гипотенуза f = sqrt(d^2 + e^2)
+let d: Double = 2.5
+let e: Double = 3.5
+
+let S = d*e/2
+let f = sqrt(d*d + e*e)
+let P = d + e + f
+
+print("Площадь треугольника = \(S)")
+print("Периметр треугольника = \(P)")
+print("Гипотенуза треугольника = \(f)")
+
+//3. Пользователь вводит сумму вклада в банк и годовой процент. Найти сумму вклада через                             5 лет.
+// sumDep - сумма вклада
+//yearPercent - годовой процент
+// timeDep - срок вклада
+let sumDep: Double = 1000
+let yearPercent: Double = 10
+let timeDep: Int = 5
+var curDep: Double = 1000
+
+for _ in 1...timeDep {
+    curDep = curDep + curDep*yearPercent/100
+}
+
+print("Через \(timeDep) лет, пользователь получит \(curDep)")
+
